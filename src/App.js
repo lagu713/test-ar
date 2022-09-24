90cc9d2d33b9921dc1f5f2bc874e593318b81104
unknown <uhdanielg@gmail.com>
2022-09-23 21:55:47 -0500

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import * as THREE from 'three'
+import * as THREEAR from 'threear'
 
 function App() {
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+  camera.position.z = 5;
+
+  var renderer = new THREE.WebGLRenderer({
+      // antialias	: true,
+      alpha: true
+    });
+
+  var markerGroup = new THREE.Group();
+  scene.add(markerGroup);
+
+  var source = new THREEAR.Source({ renderer, camera });
+
+  THREEAR.initialize({ source: source }).then((controller) => {
+    /* Three.js code here (mesh, materials... to be attached to the marker)*/
+    var path = './data/patt.hiro';
+    var patternMarker = new THREEAR.PatternMarker({
+          patternUrl: path,
+          markerObject: markerGroup
+        });
+    controller.trackMarker(patternMarker);
+
+    requestAnimationFrame(function animate(nowMsec){
+      // measure time
+      // your measure time code here
+      renderer.render( scene, camera );
+      controller.update( source.domElement );
+
+      // keep looping
+      requestAnimationFrame( animate );
+      });
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div></div>
   );
 }
 
